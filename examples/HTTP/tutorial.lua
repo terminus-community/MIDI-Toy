be99f531d6ff2d84099b74f6ca4c4cc3916fb774
Victor Gridnevsky <vic.6r1d@gmail.com>
2020-10-30 10:38:49 +0300

    Updates Lua example a bit

--- examples/HTTP/tutorial.lua
+++ examples/HTTP/tutorial.lua
@@ -1,20 +1,31 @@
--- This code is probably very bad
--- and I don't know how to make it faster.
--- If you know how to make it faster *in Lua*, please tell me.
+-- This code is probably very bad, because the requests are slow.
+--
+-- I don't know how to make those faster.
+-- Some non-blocking requests, ignoring the response will help a lot.
+-- If you happen to know how to make it faster *in Lua*, please tell me.
 
 -- Required for "sleep" function
 -- local socket = require("socket")
+
 local http = require("socket.http")
 
 local min_note = 22
 local max_note = 106
 local note_delay = 30
+local velocity = 60
+
+local note_req = function(mode, note, velocity)
+    http.request({
+        url='http://192.168.4.1/on?pitch=' .. note .. '&velocity=30',
+        method='head',
+        redirect=false
+    })
+    -- Delay is not really needed, http in Lua seems sleepy enough.
+    -- socket.sleep(note_delay / 1000)
+end
 
 for note = min_note, max_note do
-   print('Note: ' .. note)
-   http.request('http://192.168.4.1/on?pitch=' .. note .. '&velocity=30')
-   -- Delay is not really needed, http in Lua seems sleepy enough.
-   -- socket.sleep(note_delay / 1000)
-   http.request('http://192.168.4.1/off?pitch=' .. note .. '&velocity=30')
-   -- socket.sleep(note_delay / 1000)
+    print('Note: ' .. note)
+    note_req('on', note, velocity)
+    note_req('off', note, velocity)
 end
